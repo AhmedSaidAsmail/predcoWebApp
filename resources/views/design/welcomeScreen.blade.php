73d6744b327deb011178f3077daad0a2f7233df6
AhmedSaidAsmail <info.matrixcode@gmail.com>
2019-11-01 18:33:48 +0200

    welcome screen first commit

---
+++ resources/views/design/welcomeScreen.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Predco</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="title" content="">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="{{asset('images/logo.png')}}">
+    <meta property="og:site_name" content="predco.com">
+    <meta property="og:description" content="">
+    <meta property="fb:pages" content="">
+    <meta name="p:domain_verify" content="">
+    <meta name="apple-itunes-app" content="">
+    <meta name="google-play-app" content="">
+    <link href="" rel="publisher"/>
+    <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon">
+    {{-- styles --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"
+          integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+
+</head>
+<body>
+<div class="top-nav">
+    <div class="container">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="{{asset('images/logo.png')}}" alt="">
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span class="symbol"><i class="fas fa-home"></i></span>
+                    <span>بيع</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span class="symbol"><i class="fas fa-store-alt"></i></span>
+                    <span>تجارية</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span class="symbol"><i class="fas fa-hotel"></i></span>
+                    <span>مشاريع جديدة</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span class="symbol"><i class="fas fa-calculator"></i></span>
+                    <span>تقسيط</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span class="symbol"><i class="fas fa-bullhorn"></i></span>
+                    <span>اضف اعلانك</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="banner">
+    <img src="{{asset('images/cover.jpg')}}" alt="">
+    <div class="main-nav">
+        <div class="container">
+            <nav class="navbar">
+                <ul class="nav contact-side">
+                    <li class="nav-item">
+                        <a class="nav-link login" href="#">
+                            <i class="fas fa-users"></i>
+                            <span>تسجيل الدخول</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link social facebook" href="#"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link social twitter" href="#"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link social youtube" href="#"><i class="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
+                <ul class="nav justify-content-end main-side">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">الرئيسية</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">اخبار</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">فيديوهات</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">البومات</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">المعارض</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">اتصل بنا</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
